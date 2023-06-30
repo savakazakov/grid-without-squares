ebd8324a485eb4cf8161bfa3c031bd6af4258dc0
Kier Palin <Palink@lancaster.ac.uk>
2023-06-30 19:30:25 +0100

    Fixed bug with get_heat_map() initial map being empty, improved grammar in description.

--- Erlang/HCA/generate_squares.erl
+++ Erlang/HCA/generate_squares.erl
@@ -1,28 +1,30 @@
 -module(generate_squares).
--export([get_squares/1, get_heat_map/1, get_dependency_map/2, convertToBitlist/2, convertAllToBitlist/2]).
+-export([get_squares/1, get_heat_map/2, get_dependency_map/2, apply_dependency_map/2, 
+		 convertToBitlist/2, convertAllToBitlist/2]).
 
 -import(lists, [nth/2, zip/2, seq/2, duplicate/2]).
 -import(math, [pow/2]).
 
 
-% A Square can be defined by a single integer,
-% This integer will, when expressed as a binary, be entirely cleared bits, 
+% A Square can be defined by a single integer.
+% This integer will, when expressed as a binary, be consist of entirely cleared bits, 
 % Except for 4 set bits in indices corresponding to the corners of the square.
 
-% A heatmap refers to adding all of these squares together, hence creating a super imposed grid,
-% Where the values in each cell of the grid refer to the number of times that cell is a corner to a square.
-% This can be used to determine the cells that are most used & hence would be most efficiently set to 0.
+% A heatmap refers to adding all of these squares together, hence creating a super-imposed grid.
+% The values in each cell of the grid refer to the number of times that cell is a corner in a square.
+% This can be used to determine the cells that are most used & hence would be most efficient to clear.
 
-% These squares can also be combined together via bitwise OR
-% If this all of the squares that contain a 1 in a cell are OR'd together a dependency_map is created
+% These squares can also be combined together via bitwise OR.
+% If all of the squares that contain a 1 in a cell are OR'd together a dependency_map is created,
+% "Dependency" refers to the fact that all of squares in the map use that cell.
 % If a cell in the grid is set to 0, the heatmap can be updated by simply subtracting that dependency_map from the heatmap.
+
 
 
 
 %%---------------------%%
 % Interfacing Functions %
 %%---------------------%%
-
 
 % Iterate across all the points in the grid; with a scale between 2 and the gridlength:
 get_squares(GridLength) ->
@@ -38,21 +40,23 @@ construct_Square(Index, Scale, GridLength) ->
 	trunc(math:pow(2, Index - 1 + (GridLength * (Scale - 1)) + Scale - 1)).
 
 
-get_heat_map(Squares) -> get_heat_map(Squares, []).
-get_heat_map([Square |[]], Map) -> elementwise_add_list(Square, Map);
-get_heat_map([Square | Squares], Map) -> get_heat_map(Squares, elementwise_add_list(Square, Map)).
+get_heat_map(Squares, GridSize) when is_integer(GridSize) -> get_heat_map(Squares, lists:duplicate(GridSize, 0));
+get_heat_map([Square |[]], Map) -> elementwise_add_lists(Square, Map);
+get_heat_map([Square | Squares], Map) -> get_heat_map(Squares, elementwise_add_lists(Square, Map)).
 
 
 % Build this mythical, magical, magnificent map:
 get_dependency_map(Squares, GridSize) -> [combine_squares(SharedSquares, GridSize) || SharedSquares <- [lists_with_set_N(Squares, N) || N <- lists:seq(1, GridSize)]].
-combine_squares(Squares, GridSize) -> lists:foldl(fun elementwise_bor_list/2, lists:duplicate(GridSize, 0), Squares). % Bitwise OR all of the squares together
+combine_squares(Squares, GridSize) -> lists:foldl(fun elementwise_bor_lists/2, lists:duplicate(GridSize, 0), Squares). % Bitwise OR all of the squares together
+
+apply_dependency_map(HeatMap, DependencyMap) -> elementwise_sub_lists(HeatMap, DependencyMap).
+
 
 
 
 %%-----------------%%
 % Utility Functions %
 %%-----------------%%
-
 
 % Functions for get_square:
 validSquare(Index, Scale, GridLength) -> (not rowCrossing(Index, Scale, GridLength)) and (squareWithinBounds(Index, Scale, GridLength)).
@@ -65,8 +69,9 @@ convertToBitlist(Value, GridSize) -> [Bit || <<Bit:1>> <= <<Value:GridSize>>].
 convertAllToBitlist(Values, GridSize) -> [[Bit || <<Bit:1>> <= <<Value:GridSize>>] || Value <- Values].
 
 
-elementwise_bor_list(List1, List2) -> [X bor Y || {X, Y} <- lists:zip(List1, List2)].
-elementwise_add_list(List1, List2) -> [X + Y || {X, Y} <- lists:zip(List1, List2)].
+elementwise_bor_lists(List1, List2) -> [X bor Y || {X, Y} <- lists:zip(List1, List2)].
+elementwise_sub_lists(List1, List2) -> [X - Y || {X, Y} <- lists:zip(List1, List2)].
+elementwise_add_lists(List1, List2) -> [X + Y || {X, Y} <- lists:zip(List1, List2)].
 
 
 % Filter Lists, return a sublist of lists that have their Nth element equal to 1: 
